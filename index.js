e87cac96deaa0609ed281482b053277265ac9199
Dominic Smith <code@smithamax.me>
2014-04-08 13:24:31 +1000

    fix minor errors

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ var fs = require('fs');
 
 var tail = fs.readFileSync(__dirname + '/payload.js');
 
+var getScripts = function (options, callback) {
+  if (options.glob === undefined) {
+    return callback(new Error('option glob is required'));
+  }
+
+  glob(options.glob, { cwd: options.cwd }, function (err, files) {
+    if (err) { return callback(err); }
+
+    files = files.map(function (filename) {
+      return '/' + filename;
+    });
+
+    callback(null, files);
+  });
+};
+
 module.exports = function (options) {
   return function (req, res, next) {
     if (!options.path || req.url === options.path) {
@@ -17,20 +33,5 @@ module.exports = function (options) {
   };
 };
 
-var getScripts = module.exports._getScripts = function (options, callback) {
-  if (options.glob === undefined) {
-    return callback(new Error('option glob is required'));
-  }
-
-  glob(options.glob, { cwd: options.cwd }, function (er, files) {
-    if (er) { return callback(err); }
-
-    files = files.map(function (filename) {
-      return '/' + filename;
-    });
-
-    callback(null, files);
-  });
-};
-
+module.exports._getScripts = getScripts;
 module.exports._tail = tail;
